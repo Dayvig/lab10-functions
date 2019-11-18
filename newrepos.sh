6081fe036f6ee221402576f45ff31f395c937970
Edwins27 <officialedwinsamaiego@gmail.com>
2019-11-18 15:16:47 -0500

    Added four things

---
+++ newrepos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ~/maxs
+git clone https://github.com/dayvig/lab-11-canvas.git
+cd lab-11-canvas
+nano README.md
+git add README.md
+git commit -m "Added readme"
+git push origin master
+
